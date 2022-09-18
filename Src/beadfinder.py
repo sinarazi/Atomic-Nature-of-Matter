7c6ee8fc45ea3edcbbf0f4ef86881047c3beb391
sinarazi <razisina78@gmail.com>
2022-09-18 16:14:02 +0430

    just testing visual studio 2022

--- Src/beadfinder.py
+++ Src/beadfinder.py
@@ -1,6 +1,12 @@
+
 from utils import *
+from utils import Picture
 from blob import Blob
 import os
+import cv2
+#import numpy as np
+
+
 
 
 def create2D(rowCount, colCount, value=None):
@@ -27,10 +33,17 @@ class beadfinder:
         """
 
         self._blobs = [] # Initialize an empty list for the blobs in pic.    
-        marked = create2D(pic.width(), pic.height(), False) # the same dimension as pic
-  
-        for i in range(pic.width()):
-            for j in range(pic.height()):
+       # marked = create2D(pic.width(), pic.height(), False) # the same dimension as pic
+        # for i in range(pic.width()):
+        #     for j in range(pic.height()):
+        #         b = Blob()
+        #         self.Blobfinder(pic, tau, i, j, marked, b)
+        #         if b.mass() > 0:
+        #             self._blobs += [b]
+        row, col, _ = pic.shape
+        marked = create2D(row, col, False)
+        for i in range(row):
+            for j in range(col):
                 b = Blob()
                 self.Blobfinder(pic, tau, i, j, marked, b)
                 if b.mass() > 0:
@@ -49,8 +62,12 @@ class beadfinder:
         # Base case: return if pixel (i, j) is out of bounds, or if it
         # is marked, or if its luminance is less than tau.
         
-        if i >= Pic.width() or j >= Pic.height() or i < 0 or j < 0 \
-           or marked[i][j] is True or luminance(Pic.get(i, j)) < Tau:
+        # if i >= Pic.width() or j >= Pic.height() or i < 0 or j < 0 \
+        #    or marked[i][j] is True or luminance(Pic.get(i, j)) < Tau:
+        #     return
+        row, col, _ = Pic.shape
+        if i >= row or j >= col or i < 0 or j < 0 \
+           or marked[i][j] is True or luminance(Picture.get(i, j)) < Tau:
             return
 
        
@@ -89,15 +106,22 @@ def createDataset(dataset, P, Tau):
 
 def _main():
     
+    # P = 25
+    # Tau = 180.0
+    # dataset = r'F:/My projects/Atomic-Nature-of-Matter/Dataset/'
+    # for i in os.listdir(dataset):
+    #     Pic = Picture('frame00000.jpg')
+    #     b = beadfinder(Pic, Tau)
+    #     Beads = b.getBeads(P)
+    #     writeFile(Beads, "beadfinder_output/output.txt")
     P = 25
     Tau = 180.0
     dataset = r'F:/My projects/Atomic-Nature-of-Matter/Dataset/'
     for i in os.listdir(dataset):
-        Pic = Picture('frame00000.jpg')
+        Pic = cv2.imread(dataset + 'frame00000.jpg')
         b = beadfinder(Pic, Tau)
         Beads = b.getBeads(P)
         writeFile(Beads, "beadfinder_output/output.txt")
-
        
 
 if __name__ == '__main__':
